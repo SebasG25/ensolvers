c9f19934259d07304e7b8f3ab7bf89033dca66cc
Sebastian Guzman <bitas200225@gmail.com>
2022-08-18 03:34:29 -0500

    heroku push

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 18162);
   logger.log(`Server running on ${await app.getUrl()}`);
 }
 bootstrap();
